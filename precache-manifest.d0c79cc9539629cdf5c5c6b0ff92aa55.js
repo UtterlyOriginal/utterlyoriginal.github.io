dd087d4464629c9e1a83cdbc48fe7de709ead2dd
Nathan Nicholson <utterlyoriginal@gmail.com>
2020-08-25 21:15:22 -0500

    Updates

---
+++ precache-manifest.d0c79cc9539629cdf5c5c6b0ff92aa55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd9afae77f13e8dcd2760fd09af96b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f00d5eeddc6f3f5056ab",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "82c5773c6b192ce14e7f",
+    "url": "/static/js/2.7da098b1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7da098b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f00d5eeddc6f3f5056ab",
+    "url": "/static/js/main.4f51ef07.chunk.js"
+  },
+  {
+    "revision": "6d1723bcf00696a63c10",
+    "url": "/static/js/runtime-main.42f90601.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
